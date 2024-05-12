fb67af2618f532abb0f63b65febeedf08c1ef18d
itsreimau <private.iblmln@gmail.com>
2024-05-12 08:46:40 +0700

    fix: regex

--- app/public/api/ai/chatgpt.php
+++ app/public/api/ai/chatgpt.php
@@ -36,7 +36,7 @@ if (!empty($data->query) && !empty($data->appPackageName) && !empty($data->messe
     if (isset($_SERVER["HTTP_EXPERIMENTAL"]) && $_SERVER["HTTP_EXPERIMENTAL"] === "true") {
         if (isset($_SERVER["HTTP_REGEX"])) {
             $regexPattern = $_SERVER["HTTP_REGEX"];
-            if (preg_match('/' . $regexPattern . '/', $message, $matches)) {
+            if (preg_match($regexPattern, $message, $matches)) {
                 $capturingGroup1 = isset($_SERVER["HTTP_CPTGRP1"]) ? $_SERVER["HTTP_CPTGRP1"] : 1;
                 $argument1 = isset($matches[$capturingGroup1]) ? trim($matches[$capturingGroup1]) : '';
                 $response = getChatGPTResponse($argument1);
